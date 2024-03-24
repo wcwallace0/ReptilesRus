ad6f47997d2c74cc660e41a72ae61c16b5eef2ab
ddokupil12 <redder.surgery0e@icloud.com>
2024-03-23 21:41:35 -0500

    updated port to 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // Set view engine
 app.set("view engine", "ejs");
