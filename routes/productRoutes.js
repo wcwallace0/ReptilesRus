9e3b6b27068308b55406c24e98c29f25fcf02d77
Choudhry Abdullah <choudhry347@choudhrys-mbp.trinity.local>
2024-04-17 14:16:20 -0500

    Pulls data from database

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,7 @@
 const express = require('express');
+const { loadProducts } = require('../controllers/productsController');
 const router = express.Router();
 
-router.get('/products', (req, res) => {
-    const products = [
-        { name: "Product 1", description: "Description 1", price: 10 },
-        { name: "Product 2", description: "Description 2", price: 20 }
-    ];
-    res.render('products', { products });
-});
+router.get('/products', loadProducts)
 
 module.exports = router;
