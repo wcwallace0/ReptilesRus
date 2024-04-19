2955bf04621a4bcf7102d46471f59f3de3946845
ddokupil12 <redder.surgery0e@icloud.com>
2024-04-19 12:45:16 -0500

    Checkout page connected to database, missing functionality

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -9,32 +9,26 @@ const cartModel = require('../models/cartModel');
 // }));
 
 async function getUserCart(req, res) {
-   cart = await cartModel.getCart('ddokupil@trinity.edu');
+    const session = req.session;
     try {
-        if (cart) {
-            // Successful login, redirect to a dashboard or profile page
-            console.log("cart controller true");
-            console.log(cart);
-            res.render('cart', {cart});
-        } else {
-            // Username or password is incorrect, redirect back to the login page
-            res.send('no cart');
-        }
+        const cart = await cartModel.getCart(session['secret']);
+        // Successful login, redirect to a dashboard or profile page
+        console.log("cart controller true");
+        console.log(cart);
+        res.render('cart', {cart});
     } catch (error) {
-        // Handle database query errors
-        console.error('Error querying database:', error);
-        res.status(500).send('Internal Server Error');
+        // Username or password is incorrect, redirect back to the login page
+        console.log(error);
+        res.send('no cart');
     }
 }
 
 async function checkout(req, res){
-    const session = req.body.session;
+    const session = req.session;
     try {
-        if (session !== undefined && session.isPopulated){
-            const cart = await cartModel.getCart(session['secret']);
-            console.log(cart);
-            res.render('checkout', {cart});
-        }
+        const cart = await cartModel.getCart(session['secret']);
+        console.log(cart);
+        res.render('checkout', {cart});
     } catch(error){
         console.log(error);
         res.render('checkout', {});
