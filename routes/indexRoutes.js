48e183da0683ed7c3f6c09e3b8c6d07a442e7de5
LeviKle <leuykhiem@gmail.com>
2024-05-06 16:30:14 -0500

    added nav bar

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -5,4 +5,24 @@ router.get('/', (req, res) => {
     res.render('index', {});
 });
 
+router.get('/contact', (req,res) => {
+  res.render("contact")
+})
+
+router.post('/submitContactUs', (req, res) => {
+  console.log(req.body)
+  //do something about this
+  res.render("contact")
+})
+
+router.get('/aboutUs', (req,res) => {
+  res.render("aboutUs")
+})
+
+router.get('/services' , (req,res) => {
+  res.render("services")
+})
+
+
+
 module.exports = router;
